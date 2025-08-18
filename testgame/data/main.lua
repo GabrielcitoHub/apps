234881357d1a24dfddd5f2f243d1fca2fa4e93f0
GabrielcitoHub <82218456+GabrielcitoHub@users.noreply.github.com>
2025-08-18 20:52:16 -0300

    trying fixes

--- testgame/data/main.lua
+++ testgame/data/main.lua
@@ -1,5 +1,5 @@
 require "lib.wii"
 
 function love.draw()
-	love.graphics.print("WiiLÖVE is awesome!", love.wiimote.getX(1) or 0, love.wiimote.getY(1) or 0)
+	love.graphics.print("WiiLÖVE is awesome!", love.wiimote.getX(1) or 320, love.wiimote.getY(1) or 240)
 end
