af477a60fe9c54313da16d8860fbc8fc7e16604d
GabrielcitoHub <82218456+GabrielcitoHub@users.noreply.github.com>
2025-08-18 18:52:12 -0300

    testgame test for testing test things

---
+++ testgame/data/main.lua
@@ -0,0 +1,5 @@
+require "lib.wii"
+
+function love.draw()
+	love.graphics.print("WiiLÖVE is awesome!", 320, 240)
+end
---
+++ testgame/data/modules/wiimote.lua
@@ -0,0 +1,32 @@
+local Wiimote = {}
+
+function Wiimote.getWiimote(id)
+    local wiimote = {}
+    wiimote.id = id
+
+    wiimote.angle = love.wiimote.getAngle(wiimote.id)
+    wiimote.extension = love.wiimote.getExtension(wiimote.id)
+    wiimote.position = love.wiimote.getPosition(wiimote.id)
+    wiimote.x = love.wiimote.getX(wiimote.id)
+    wiimote.y = love.wiimote.getY(wiimote.id)
+    wiimote.connected = love.wiimote.isConnected(wiimote.id)
+    wiimote.down = love.wiimote.isDown(wiimote.id)
+    wiimote.rumbling = love.wiimote.isRumbling(wiimote.id)
+    wiimote.classicDown = love.wiimote.isClassicDown(wiimote.id)
+    wiimote.setRumble = love.wiimote.setRumble(wiimote.id)
+
+    return wiimote
+end
+
+function Wiimote.getWiimotes()
+    local wiimotes = {}
+
+    for i=1,4 do
+        table.insert(wiimotes,Wiimote.getWiimote(i))
+    end
+
+    return wiimotes
+end
+
+
+return Wiimote
