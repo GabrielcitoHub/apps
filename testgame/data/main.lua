7809909bf73393f5ef7b6dcf37190d2a5c9324b6
GabrielcitoHub <82218456+GabrielcitoHub@users.noreply.github.com>
2025-08-18 19:24:35 -0300

    small fixes

--- testgame/data/main.lua
+++ testgame/data/main.lua
@@ -1,5 +1,5 @@
 require "lib.wii"
 
 function love.draw()
-	love.graphics.print("WiiLÖVE is awesome!", wiimote.getWiimotes[1].x, wiimote.getWiimotes[1].y)
+	love.graphics.print("WiiLÖVE is awesome!", wiimote.getWiimotes()[1].x or 0, wiimote.getWiimotes()[1].y or 0)
 end
--- testgame/data/modules/wiimote.lua
+++ testgame/data/modules/wiimote.lua
@@ -2,8 +2,8 @@ local Wiimote = {}
 
 function Wiimote.getWiimote(id)
     local wiimote = {}
-    wiimote.id = id
 
+    wiimote.id = id
     wiimote.angle = love.wiimote.getAngle(wiimote.id)
     wiimote.extension = love.wiimote.getExtension(wiimote.id)
     wiimote.position = love.wiimote.getPosition(wiimote.id)
