519259736e47a43b4434f2cfbb048261f261ebd4
GabrielcitoHub <82218456+GabrielcitoHub@users.noreply.github.com>
2025-08-18 19:09:39 -0300

    testing something with wiimote

--- testgame/data/main.lua
+++ testgame/data/main.lua
@@ -1,5 +1,5 @@
 require "lib.wii"
 
 function love.draw()
-	love.graphics.print("WiiLÖVE is awesome!", 320, 240)
+	love.graphics.print("WiiLÖVE is awesome!", wiimote.getWiimotes[1].x, wiimote.getWiimotes[1].y)
 end
