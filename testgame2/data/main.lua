a1c4d5e4b27921d2c41198bac7a8cef45c55cb9f
GabrielcitoHub <82218456+GabrielcitoHub@users.noreply.github.com>
2025-08-22 20:50:21 -0300

    WORK GOD DAMMIT!!!

--- testgame2/data/main.lua
+++ testgame2/data/main.lua
@@ -2,6 +2,6 @@ require "lib.wii"
 
 function love.draw()
 	love.graphics.print(tostring("Wiimote 1 X: " .. love.wiimote.getX(0) .. " Y: " .. love.wiimote.getY(0)),  320, 240)
-	love.graphics.print(tostring("Wiimote 1 with GW X: " .. wiimote.getWiimote(0).x .. " Y: " .. wiimote.getWiimote(0).y),  320, 210)
-	love.graphics.print(tostring("Wiimote 1 with GWS X: " .. wiimote.getWiimotes()[1].x .. " Y: " .. wiimote.getWiimotes()[1].y),  320, 180)
+	love.graphics.print(tostring("Wiimote 1 GW X: " .. wiimote.getWiimote(0).x .. " Y: " .. wiimote.getWiimote(0).y),  320, 210)
+	love.graphics.print(tostring("Wiimote 1 GWS X: " .. wiimote.getWiimotes()[1].x .. " Y: " .. wiimote.getWiimotes()[1].y),  320, 180)
 end
--- wiindertale/data/main.lua
+++ wiindertale/data/main.lua
@@ -1,13 +1,10 @@
 require "lib.wii"
 
-local cursorsimg
-local plr1cursor
-
 function love.load()
-    cursorsimg = love.graphics.newImage("images/cursors.png")
-    plr1cursor = love.graphics.newQuad(0,0,16,16)
+    cursor = love.graphics.newTexture("cursor.png")
 end
 
 function love.draw()
-    love.graphics.drawQuad(cursorsimg, plr1cursor, wiimote.getWiimote(0).x, wiimote.getWiimote(0).y, wiimote.getWiimote(0).angle, 1, 1, 8, 8)
+    love.graphics.print(tostring("Wiimote 1 GW X: " .. wiimote.getWiimote(0).x .. " Y: " .. wiimote.getWiimote(0).y),  320, 210)
+    love.graphics.draw(cursor, wiimote.getWiimote(0).x, wiimote.getWiimote(0).y, 0, 1, 1, 8, 8)
 end
